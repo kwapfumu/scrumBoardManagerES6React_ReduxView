ec66e35d197a911765bbde5edec04326b9c5deff
kwapfumu <rigumyel@gmail.com>
2017-08-03 21:43:37 +0200

    Add files via upload

---
+++ client/src/app/reusableComponents/UrlNotFound.js
@@ -0,0 +1,14 @@
+"use strict";
+import 'babel-polyfill';
+import React, { Component } from 'react';
+import {render} from 'react-dom';
+
+class UrlNotFound extends Components{
+	render(){
+		return(
+			<h1>URL not Found</h1>
+		);
+	};
+};
+
+export default UrlNotFound;
