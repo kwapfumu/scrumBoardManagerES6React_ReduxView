01f0ad39a87d97730814213244be9479241a6a9c
kwapfumu <rigumyel@gmail.com>
2017-08-07 10:49:40 +0200

    Add files via upload

--- client/src/app/admin/actions/EditFormModalActionsCreators/showEditFormModalActionsCreator.js
+++ client/src/app/admin/actions/EditFormModalActionsCreators/showEditFormModalActionsCreator.js
@@ -2,6 +2,7 @@
 import fetch from 'isomorphic-fetch';
 import * as types from '../../../constants/constants';
 import * as api from '../../../constants/ApiConstants';
+import fetchResponseHandlerFctr from '../../../fetchResponseHandlerFctr';
 
 function openAdminEditFormModalActionCreator(aTaskId){ 
 	return{
@@ -27,8 +28,8 @@ function openAdminEditFormModalFailedActionCreator(anError){
 function showEditFormModalActionsCreator(aTaskId){
 	return (dispatch) => { 
 		dispatch(openAdminEditFormModalActionCreator(aTaskId));
-		return fetch(`${api.API_URL}/admin/releaseBacklog/:taskId/edit`, {headers:api.API_HEADERS})
-				.then(checkHttpErrorStatus)
+		return fetch(`${api.API_URL}/admin/releaseBacklog/${aTaskId}`, {headers:api.API_HEADERS})
+				.then(fetchResponseHandlerFctr.checkHttpErrorStatus)
 				.then((response) => response.json())
 				.then((json) => dispatch(openedAdminEditFormModalActionCreator(json)))
 				.catch((error) => {
