a4a229c555943b504a96d1bdfae2afd0dedbb941
kwapfumu <rigumyel@gmail.com>
2017-08-07 10:53:15 +0200

    Add files via upload

--- client/src/app/admin/reducers/adminFeaturesReducers/showEditFormModalReducers/showEditFormModalReducer.js
+++ client/src/app/admin/reducers/adminFeaturesReducers/showEditFormModalReducers/showEditFormModalReducer.js
@@ -4,9 +4,6 @@ import {showedEditFormModalReducer} from './showedEditFormModalReducer';
 import {showEditFormModalFailedReducer} from './showEditFormModalFailedReducer';
 
 /**slice reducer: a reducer that is being used to handle updates to one specific slice of the state tree*/
-/**a reducer is a pure function. It only computes the next state. It should be completely predictable: 
-calling it with the same inputs many times should produce the same outputs. It shouldn't perform any 
-side effects like API calls or router transitions. These should happen before an action is dispatched.*/
 export function showEditFormModalReducer(adminEditFormModalState = { 
 	isFetching: false,
 	aTask2Edit: {}
