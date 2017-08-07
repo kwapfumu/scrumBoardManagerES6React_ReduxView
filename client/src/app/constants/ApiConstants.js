58fdedca8eaba1fb1cc48031d8018b4072b45047
kwapfumu <rigumyel@gmail.com>
2017-08-07 10:56:29 +0200

    Add files via upload

--- client/src/app/constants/ApiConstants.js
+++ client/src/app/constants/ApiConstants.js
@@ -1,7 +1,7 @@
 export default {
-	API_URL = 'http://kanbanapi.pro-react.com',
+	API_URL = 'http://scrumboardmern.com',
 	API_HEADERS: {
 		'Content-Type': 'application/json',
-		Authorization: 'any-string-you-like'
+		'Authorization': 'any-string-you-like'
 	},
 };
